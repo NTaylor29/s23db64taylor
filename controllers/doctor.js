6dfad11f5011eee65e5490a2bd45cdafe0d711e6
Nicholas Taylor <70026110+NTaylor29@users.noreply.github.com>
2023-04-19 15:54:06 -0500

    fixed resource

--- controllers/doctor.js
+++ controllers/doctor.js
@@ -87,3 +87,16 @@ exports.doctor_update_put = async function(req, res) {
     res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
  }
 };
+
+// Handle a show one view with id specified by query
+exports.doctor_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+        result = await Doctor.findById( req.query.id)
+        res.render('doctordetail', { title: 'Doctor Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};

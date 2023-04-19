339bddd8d9c37eb7084424fe2857d3f26c4f0b22
Nicholas Taylor <70026110+NTaylor29@users.noreply.github.com>
2023-04-19 16:30:17 -0500

    added doctor create

--- routes/doctors.js
+++ routes/doctors.js
@@ -4,6 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', doctor_controlers.doctor_view_all_Page );
-/* GET detail costume page */
+/* GET detail doctor page */
 router.get('/detail', doctor_controlers.doctor_view_one_Page);
+/* GET create doctor page */
+router.get('/create', doctor_controlers.doctor_create_Page);
 module.exports = router;

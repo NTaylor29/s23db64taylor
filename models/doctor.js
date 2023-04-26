fd8c09e6a3ce7b4abc0fbf21666e7e629bcd3703
Nicholas Taylor <70026110+NTaylor29@users.noreply.github.com>
2023-04-26 17:48:13 -0500

    final commit

--- models/doctor.js
+++ models/doctor.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 const doctorSchema = mongoose.Schema({
     name: String,
-    age: Number,
+    age: {
+        type: Number,
+        min: [18, "too young"]
+    },
     location: String
 })
 module.exports = mongoose.model("Doctor", doctorSchema)
